261499ae0c2ecd3db49b6978734621b4c89d9f1d
Tim Blankers <timblankers@gmail.com>
2016-10-04 23:27:45 +0200

    rename class, remove comments

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ To build the peer, run in this directory:
 $ ./gradlew installDist
 ```
 
-This creates the script `hello-world-client`.
+This creates the script `fabric-client`.
 
 To launch a regular Go Fabric peer, first run:
 
@@ -21,19 +21,13 @@ $ docker-compose up
 Then try the java peer:
 
 ```
-$ ./build/install/examples/bin/hello-world-client
+$ ./build/install/examples/bin/fabric-client
 ```
-
-That's it!
-
-Please refer to gRPC Java's [README](../README.md) and
-[tutorial](http://www.grpc.io/docs/tutorials/basic/java.html) for more
-information.
 
 ## Maven
 
 If you prefer to use Maven:
 ```
 $ mvn verify
-$ mvn exec:java -Dexec.mainClass=io.grpc.examples.helloworld.HelloWorldClient
+$ mvn exec:java -Dexec.mainClass=io.grpc.fabric.FabricClient
 ```
--- src/main/java/io/grpc/fabric/FabricClient.java
+++ src/main/java/io/grpc/fabric/FabricClient.java
@@ -1,34 +1,3 @@
-/*
- * Copyright 2015, Google Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *    * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *
- *    * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package io.grpc.fabric;
 
 import io.grpc.ManagedChannel;
@@ -42,20 +11,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.time.Instant;
 
-/**
- * A simple client that requests a greeting from the {@link HelloWorldServer}.
- */
 public class FabricClient {
   private static final Logger logger = Logger.getLogger(FabricClient.class.getName());
 
   private final ManagedChannel channel;
   private final PeerGrpc.PeerBlockingStub blockingStub;
 
-  /** Construct client connecting to HelloWorld server at {@code host:port}. */
   public FabricClient(String host, int port) {
     channel = ManagedChannelBuilder.forAddress(host, port)
-        // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
-        // needing certificates.
         .usePlaintext(true)
         .build();
     blockingStub = PeerGrpc.newBlockingStub(channel);
@@ -65,7 +28,7 @@ public class FabricClient {
     channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
   }
 
-  public void greet() {
+  public void discover() {
     Instant time = Instant.now();
     Message request =
         Message.newBuilder()
@@ -79,13 +42,13 @@ public class FabricClient {
       logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
       return;
     }
-    logger.info("Greeting: " + response.getType());
+    logger.info("Response: " + response.getType());
   }
 
   public static void main(String[] args) throws Exception {
     FabricClient client = new FabricClient("localhost", 7051);
     try {
-      client.greet();
+      client.discover();
     } finally {
       client.shutdown();
     }
