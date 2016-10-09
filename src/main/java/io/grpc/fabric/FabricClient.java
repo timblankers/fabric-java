339fda8bcd43b14eedbea753394117920a359329
Tim Blankers <timblankers@gmail.com>
2016-10-09 22:58:29 +0200

    implement bidirectional stream

--- src/main/java/io/grpc/fabric/FabricClient.java
+++ src/main/java/io/grpc/fabric/FabricClient.java
@@ -2,12 +2,16 @@ package io.grpc.fabric;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 import com.google.protobuf.Timestamp;
 import com.google.protobuf.ByteString;
 
 import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.CountDownLatch;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.time.Instant;
@@ -17,19 +21,80 @@ public class FabricClient {
 
   private final ManagedChannel channel;
   private final PeerGrpc.PeerBlockingStub blockingStub;
+  private final PeerGrpc.PeerStub asyncStub;
 
   public FabricClient(String host, int port) {
     channel = ManagedChannelBuilder.forAddress(host, port)
         .usePlaintext(true)
         .build();
     blockingStub = PeerGrpc.newBlockingStub(channel);
+    asyncStub = PeerGrpc.newStub(channel);
   }
 
   public void shutdown() throws InterruptedException {
     channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
   }
 
-  public void discover() {
+  public void discover() throws Exception {
+    logger.info("[server] Starting StreamObserver...");
+    final CountDownLatch finishLatch = new CountDownLatch(1);
+    StreamObserver<Message> requestObserver =
+      asyncStub.chat(new StreamObserver<Message>() {
+        @Override
+        public void onNext(Message incoming) {
+          info("Receiving message \"{0}\" at {1}", incoming.getType(), incoming.getTimestamp());
+        }
+
+        @Override
+        public void onError(Throwable t) {
+          Status status = Status.fromThrowable(t);
+          logger.log(Level.WARNING, "Chat Failed: {0}", status);
+          finishLatch.countDown();
+        }
+
+        @Override
+        public void onCompleted() {
+          logger.info("Finished Discovery");
+          finishLatch.countDown();
+        }
+      });
+    logger.info("[server] ... StreamObserver ready");
+
+    try {
+      Message[] requests = { newMessage() };
+
+      for (Message request : requests) {
+        info("Sending message \"{0}\" at {1}", request.getType(), request.getTimestamp());
+        requestObserver.onNext(request);
+      }
+    } catch (RuntimeException e) {
+      requestObserver.onError(e);
+      throw e;
+    }
+
+    requestObserver.onCompleted();
+    finishLatch.await(1, TimeUnit.MINUTES);
+  }
+
+  // public void discover() {
+    // Iterator<Message> responses;
+    // // Collection<Message> discoveries;
+    // Message discovery = newMessage();
+    //
+    // try {
+    //   // responses = blockingStub.chat(discovery);
+    // } catch (StatusRuntimeException e) {
+    //   logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+    //   return;
+    // }
+    // while (responses.hasNext()) {
+    //   Object element = responses.next();
+    //   logger.info("[response] " + element);
+    // }
+    // logger.info("Response: " + response.getType());
+  // }
+
+  private Message newMessage() {
     ByteString bytes = ByteString.copyFromUtf8("test");
     BlockchainInfo blockchainInfo =
       BlockchainInfo.newBuilder()
@@ -43,7 +108,7 @@ public class FabricClient {
         .setName("JavaPeer")
         .build();
 
-    ByteString bytes2 = ByteString.copyFromUtf8("123");
+    ByteString bytes2 = ByteString.copyFromUtf8("insert pki id here");
     PeerEndpoint peerEndpoint =
       PeerEndpoint.newBuilder()
         .setID(peerID)
@@ -59,21 +124,15 @@ public class FabricClient {
         .build();
 
     Instant time = Instant.now();
-    Message discovery =
-      Message.newBuilder()
+    return Message.newBuilder()
         .setType(Message.Type.DISC_HELLO)
         .setTimestamp(Timestamp.newBuilder().setSeconds(time.getEpochSecond()))
         .setPayload(helloMessage.toByteString())
         .build();
+  }
 
-    Message response;
-    try {
-      response = blockingStub.chat(discovery);
-    } catch (StatusRuntimeException e) {
-      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
-      return;
-    }
-    logger.info("Response: " + response.getType());
+  private static void info(String msg, Object... params) {
+    logger.log(Level.INFO, msg, params);
   }
 
   public static void main(String[] args) throws Exception {
